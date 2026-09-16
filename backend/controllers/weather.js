d977379272cf2a7314ae5caf9c6aff892317d8ca
Kivisto Kasper <kivisto.kasper@gmail.com>
2026-09-16 21:50:07 +0300

    Working docker compose

--- backend/controllers/weather.js
+++ backend/controllers/weather.js
@@ -1,5 +1,6 @@
 const weatherRouter = require('express').Router()
 const { request } = require('express')
+const logger = require('../utils/logger');
 
 weatherRouter.get('/', async (req, res) => {
     res.json('hello from weather router!')
